eaf5095825f1a734a82fdb515bb12fa3a7370693
Corey Lutrick <coreylutrick@gmail.com>
2018-02-27 21:18:28 -0800

    challenge 3 done (confusing)

--- arrays.js
+++ arrays.js
@@ -11,3 +11,34 @@ if(test === opp){
     console.log(false);
 }
 
+//example
+
+var animal = ["fish", "dog", "cat"];
+var animalzElement = document.getElementById("animalz");
+
+for (var i=0; i<animal.length; i++){
+    console.log(animal[i]);
+    //animalzElement.innerHTML += "<h5>" +
+}
+
+//challenge 3
+
+var book1 = "Catch 22"
+var book2 = "Slaughterhouse 5"
+var book3 = "Fahrenheit 451"
+
+var Arr = book1.replace(/\D/g, "")
+for(var j=0; j<Arr.length; j++){
+    console.log(Arr[j])
+}
+
+var book = "catch 22";
+var bookSplit = book.split("");
+var realNum = [];
+for(k=0; k<bookSplit.length; k++){
+    if(+bookSplit[k]){
+        realNum.push(bookSplit[k]);
+    }
+}
+console.log(realNum.join(""))
+
